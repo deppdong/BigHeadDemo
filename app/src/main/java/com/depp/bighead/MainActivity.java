eea5d73ac778b1a9739b37c0297b35b22e8a9005
董书建 <dongshujian@meizu.com>
2018-06-04 20:05:56 +0800

    添加头像测试资源

--- app/src/main/java/com/depp/bighead/MainActivity.java
+++ app/src/main/java/com/depp/bighead/MainActivity.java
@@ -1,13 +1,54 @@
 package com.depp.bighead;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int[] sDrawableId = new int[]{
+            R.drawable.head1,
+            R.drawable.head2,
+            R.drawable.head3,
+            R.drawable.head4,
+            R.drawable.head5,
+            R.drawable.head6,
+            R.drawable.head7,
+            R.drawable.head8,
+            R.drawable.head9,
+            R.drawable.head10,
+            R.drawable.head11,
+    };
+
+
+    private static int sHeadIndex =0;
+    private ImageView mHeadImageView;
+    private ImageView mFrontLayer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHeadImageView = (ImageView) findViewById(R.id.head_img);
+        mFrontLayer = (ImageView) findViewById(R.id.front_layer);
+
+        mHeadImageView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                sHeadIndex ++;
+                if (sHeadIndex >= sDrawableId.length) {
+                    sHeadIndex = 0;
+                }
+
+                mHeadImageView.setImageResource(sDrawableId[sHeadIndex]);
+            }
+        });
+
+
     }
 }
+
